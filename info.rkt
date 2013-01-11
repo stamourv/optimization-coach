180e079e07815e09c7bbbfd702b8785c17386644
Vincent St-Amour <stamourv@racket-lang.org>
2013-01-11 17:34:48 -0500

    Add info.rkt.

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang setup/infotab
+
+(define scribblings '(("scribblings/optimization-coach.scrbl" () (tool))))
+
+(define drracket-tools '(("tool.rkt")))
+(define drracket-tool-names '("Optimization Coach"))
