b008d0ce19a650ab97254cd5a1c94c7ec0a615d0
Vincent St-Amour <stamourv@racket-lang.org>
2013-07-09 15:19:56 -0400

    Fix deps.

--- info.rkt
+++ info.rkt
@@ -2,7 +2,10 @@
 
 (define collection "optimization-coach")
 
-(define deps '("base" "profile-lib" "rackunit-lib"))
+(define deps '("base" "profile-lib" "rackunit-lib" "drracket" "gui-lib"
+               "images" "sandbox-lib" "string-constants-lib" "typed-racket-lib"
+               "unstable-list-lib" "unstable-pretty-lib"))
+(define build-deps '("scribble-lib"))
 
 (define scribblings '(("scribblings/optimization-coach.scrbl" () (tool))))
 
