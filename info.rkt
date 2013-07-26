637939fe378aeb77ba4819880f5a02619c452b3a
Vincent St-Amour <stamourv@racket-lang.org>
2013-07-26 15:48:13 -0400

    Make OC a multi-collection package.
    To be backwards compatible with 5.3.6, for which multi-collection is the default.

--- info.rkt
+++ info.rkt
@@ -1,13 +1,8 @@
 #lang info
 
-(define collection "optimization-coach")
+(define collection 'multi) ; for compatibility with 5.3.6
 
 (define deps '("base" "profile-lib" "rackunit-lib" "drracket" "gui-lib"
                "images" "sandbox-lib" "string-constants-lib" "typed-racket-lib"
                "unstable-list-lib" "unstable-pretty-lib"))
 (define build-deps '("scribble-lib"))
-
-(define scribblings '(("scribblings/optimization-coach.scrbl" () (tool))))
-
-(define drracket-tools '(("tool.rkt")))
-(define drracket-tool-names '("Optimization Coach"))
