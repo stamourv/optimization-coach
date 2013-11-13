eef9474eea0176baa60fc5518943a1429f13cd76
Vincent St-Amour <stamourv@ccs.neu.edu>
2013-11-13 11:20:47 -0500

    Update info.rkt for 5.3.6.
    Dependencies ship with main distribution, and listing them breaks installation.

--- info.rkt
+++ info.rkt
@@ -1,8 +1,3 @@
 #lang setup/infotab
 
 (define collection 'multi) ; for compatibility with 5.3.6
-
-(define deps '("base" "profile-lib" "rackunit-lib" "drracket" "gui-lib"
-               "images" "sandbox-lib" "string-constants-lib" "typed-racket-lib"
-               "unstable-list-lib" "unstable-pretty-lib"))
-(define build-deps '("scribble-lib"))
