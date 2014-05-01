34cfb004cec372c4f2195befb95dca39e4e175d9
Vincent St-Amour <stamourv@racket-lang.org>
2014-05-01 14:39:44 -0400

    Add version number.
    Arbitrary, but OC was rewritten at least twice, so...

--- info.rkt
+++ info.rkt
@@ -2,6 +2,8 @@
 
 (define collection 'multi) ; for compatibility with 5.3.6
 
+(define version "3.0")
+
 (define deps '("base" "profile-lib" "rackunit-lib" "drracket" "gui-lib"
                "data-lib" "source-syntax"
                "images-lib" "sandbox-lib" "string-constants-lib" "typed-racket-lib"
