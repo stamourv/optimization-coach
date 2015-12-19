1656fc7829720d64254cf32c45482e052b0a8acd
Vincent St-Amour <stamourv@racket-lang.org>
2015-12-19 17:54:49 -0600

    Move docs to "Performance Tools category."

--- optimization-coach/info.rkt
+++ optimization-coach/info.rkt
@@ -1,6 +1,6 @@
 #lang setup/infotab
 
-(define scribblings '(("scribblings/optimization-coach.scrbl" () (tool))))
+(define scribblings '(("scribblings/optimization-coach.scrbl" () ("Performance Tools"))))
 
 (define drracket-tools '(("tool.rkt")))
 (define drracket-tool-names '("Optimization Coach"))
