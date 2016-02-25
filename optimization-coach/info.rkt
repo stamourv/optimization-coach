5f03e2c43e276cf51fa370920874721089222498
Vincent St-Amour <stamourv@racket-lang.org>
2016-02-25 16:23:58 -0600

    Revert "Move docs to "Performance Tools category.""
    This reverts commit 1656fc7829720d64254cf32c45482e052b0a8acd.

--- optimization-coach/info.rkt
+++ optimization-coach/info.rkt
@@ -1,6 +1,6 @@
 #lang setup/infotab
 
-(define scribblings '(("scribblings/optimization-coach.scrbl" () ("Performance Tools"))))
+(define scribblings '(("scribblings/optimization-coach.scrbl" () (tool))))
 
 (define drracket-tools '(("tool.rkt")))
 (define drracket-tool-names '("Optimization Coach"))
